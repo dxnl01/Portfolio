0ed736d1869f2ac5ddedc0566951e2115771d8ad
Daniel <daniel.atehortua@utp.edu.co>
2023-08-12 23:57:35 -0500

    Correcting Problems

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import * as React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const HomePage = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="max-w-xl p-6 bg-white rounded shadow-md">
+        <h1 className="text-3xl font-semibold mb-4">¡Bienvenido a mi Portafolio!</h1>
+        <p className="text-gray-700">Soy un desarrollador web con experiencia en tecnologías frontend y backend.</p>
+        <div className="mt-6">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Ver Proyectos
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
