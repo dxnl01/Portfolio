61565cef13e116462f6832df020f12683f855a2a
Daniel <daniel.atehortua@utp.edu.co>
2023-08-13 23:41:38 -0500

    Another Testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="max-w-xl p-6 bg-white rounded shadow-md">
+    <div className="bg-white min-h-screen flex items-center justify-center">
+      <div className="max-w-xl p-6 bg-black text-white rounded shadow-md">
         <h1 className="text-3xl font-semibold mb-4">¡Bienvenido a mi Portafolio!</h1>
-        <p className="text-gray-700">Soy un desarrollador web con experiencia en tecnologías frontend y backend.</p>
+        <p className="text-gray-300">Soy un desarrollador web con experiencia en tecnologías frontend y backend.</p>
         <div className="mt-6">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          <button className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-900">
             Ver Proyectos
           </button>
         </div>
